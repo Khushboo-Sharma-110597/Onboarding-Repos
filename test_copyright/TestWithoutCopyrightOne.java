642e1306b1c9ab81afb4c0523d943ab1d22d1b82
Khushboo <Khushboo110597@ibm.com>
2026-04-30 13:17:10 +0530

    test

---
+++ test_copyright/TestWithoutCopyrightOne.java
@@ -0,0 +1,9 @@
+package test_copyright;
+
+public class TestWithoutCopyrightOne {
+    public static void main(String[] args) {
+        System.out.println("Test file one without copyright");
+    }
+}
+
+// Made with Bob
---
+++ test_copyright/TestWithoutCopyrightTwo.java
@@ -0,0 +1,9 @@
+package test_copyright;
+
+public class TestWithoutCopyrightTwo {
+    public static void main(String[] args) {
+        System.out.println("Test file two without copyright");
+    }
+}
+
+// Made with Bob
